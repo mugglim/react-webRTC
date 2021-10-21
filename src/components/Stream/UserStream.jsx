a64d54c0e69d65588572c629ae329f069aa1b133
Jaeuk Lim <applewodnr@naver.com>
2021-10-21 13:17:42 +0900

    feat: 비디오 토글 버튼

--- src/components/Stream/UserStream.jsx
+++ src/components/Stream/UserStream.jsx
@@ -6,6 +6,7 @@ const Stream = styled.div`
 	width: 300px;
 	height: 300px;
 	border: 1px solid black;
+	position: relative;
 `;
 
 const Video = styled.video`
@@ -14,12 +15,33 @@ const Video = styled.video`
 	object-fit: cover;
 `;
 
+const Button = styled.button`
+	width: 100%;
+	height: auto;
+	background-color: skyblue;
+	outline: none;
+	cursor: pointer;
+`;
+
+const ButtonWrap = styled.div`
+	position: absolute;
+	bottom: 20px;
+	left: 50%;
+	transform: translateX(-50%);
+`;
+
 export default function UserStream() {
 	const { loading, stream } = useGetStream({ audio: true, video: true });
 	const videoRef = useRef(null);
 
+	const handleVideoToggle = () => {
+		if (stream) {
+			stream.getVideoTracks().forEach(track => (track.enabled = !track.enabled));
+		}
+	};
+
 	useEffect(() => {
-		if (!loading && stream) {
+		if (!loading) {
 			videoRef.current.srcObject = stream;
 		}
 	}, [loading, stream]);
@@ -27,6 +49,10 @@ export default function UserStream() {
 	return (
 		<Stream>
 			<Video ref={videoRef} autoPlay />
+
+			<ButtonWrap>
+				<Button onClick={handleVideoToggle}>Video Toggle</Button>
+			</ButtonWrap>
 		</Stream>
 	);
 }
