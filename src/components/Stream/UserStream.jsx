fe103c171f5466be49b3e2460af084325793f371
Jaeuk Lim <applewodnr@naver.com>
2021-10-21 13:04:11 +0900

    desgin: box-sizing 전부 적용

--- src/components/Stream/UserStream.jsx
+++ src/components/Stream/UserStream.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useGetStream } from '../../hooks/stream';
 
